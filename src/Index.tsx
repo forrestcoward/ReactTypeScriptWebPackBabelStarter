537206b62ec8404a83b196969b4edd080e588266
Forrest Coward <forrest.coward@live.com>
2024-02-02 16:08:25 -0800

    Add dist folder so sample works properly

--- src/Index.tsx
+++ src/Index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { App } from './App';
-const rootNode = document.getElementById('app');
+const rootNode = document.getElementById('root');
 if (rootNode) {
   createRoot(rootNode)
     .render(<App />);
